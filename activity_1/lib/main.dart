041405d117ba077971ba0fa2357b3b6cc613f917
legalgmng-afk <legal.gmng@gmail.com>
2026-09-11 05:47:50 +0000

    Adding basic info

---
+++ activity_1/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+//Justine Paul Ignacio
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+
+  Widget build(BuildContext context){
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.favorite,
+                color: Colors.purple,
+                size: 24.0,
+                semanticLabel: 'Text to announce in accessibility modes',
+              ),
+              Text (
+                'Justine Paul T. Ignacio',
+                style: TextStyle(
+                fontWeight: FontWeight.bold, 
+                fontSize: 21
+                ),
+              ),//Text
+              Text (
+                'Bachelor in Information Technology',
+                style: TextStyle(
+                color: Colors.grey,
+                fontSize: 16,
+                ),
+              ),//Text
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Icon(
+                    Icons.favorite,
+                    color: Colors.pink,
+                    size: 24.0,
+                    semanticLabel: 'Text to announce in accessibility modes',
+                  ),
+                  Icon(
+                    Icons.audiotrack,
+                    color: Colors.green,
+                    size: 30.0,
+                  ),
+                  Icon(
+                    Icons.beach_access,
+                    color: Colors.blue,
+                    size: 36.0,
+                  ),
+                ],
+              )
+                          ],//Children
+          ),//Column
+        ),//Center
+      ),//Scaffold
+    );//MaterialApp
+  }//build
+}
+//Justine Paul Ignacio
+
